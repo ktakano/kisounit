75bbd641447a7b43b4d64dda90b81be454628a99
ktakano <takano@ic.kanagawa-it.ac.jp>
2022-09-01 10:37:57 +0900

    Improved key names.

--- ai/mnist.py
+++ ai/mnist.py
@@ -12,7 +12,7 @@ def load_mnist():
     with open(mnist_file, 'rb') as f:
         mnist = pickle.load(f)
 
-    return (mnist['train_image'], mnist['train_label']), (mnist['train_image'], mnist['test_label'])
+    return (mnist['train_image'], mnist['train_label']), (mnist['test_image'], mnist['test_label'])
 
 def to_one_hot_label(_label):
     one_hot_label = np.zeros((_label.size, NUM_CLASSES))
@@ -29,17 +29,17 @@ def save_mnist(mnist_img):
     img = Image.fromarray(np.uint8(mnist_img))
     img.save("mnist.png")
 
+if __name__ == '__main__':
+    (x_train, t_train), (x_test, t_test) = load_mnist()
 
-#(x_train, t_train), (x_test, t_test) = load_mnist()
+    mnist_img = x_train[0]
+    mnist_label = t_train[0]
 
-#mnist_img = x_train[0]
-#mnist_label = t_train[0]
-
-#mnist_img = mnist_img.astype(np.float32)
-#mnist_img = mnist_img.reshape(28, 28) 
-#print(mnist_label)
-#show_mnist(mnist_img)
-#save_mnist(mnist_img)
+    mnist_img = mnist_img.astype(np.float32)
+    mnist_img = mnist_img.reshape(28, 28) 
+    print(mnist_label)
+    show_mnist(mnist_img)
+    save_mnist(mnist_img)
 
 
 
